68cfa57190de8c46e36689e316543a3c19e9099e
IsabellaGallego <isabella.gallegoloaiza@gmail.com>
2022-12-10 20:18:58 -0500

    actualizacion total

--- API/venta-carros/src/main/java/com/tecnologica/ventacarros/irepository/IDetallesFacturasRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tecnologica.ventacarros.irepository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.tecnologica.ventacarros.collection.DetallesFacturas;
-
-public interface IDetallesFacturasRepository extends MongoRepository<DetallesFacturas, String>{
-
-}
--- Sprint 2/venta-carros/src/main/java/com/tecnologica/ventacarros/irepository/ITiposVehiculosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tecnologica.ventacarros.irepository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.tecnologica.ventacarros.collection.TiposVehiculos;
-
-public interface ITiposVehiculosRepository extends MongoRepository<TiposVehiculos, String>{
-
-}
